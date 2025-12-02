b8c70a218e854dca3484718ceb1b68fbd0274d34
ssmunkhbat <47767779+ssmunkhbat@users.noreply.github.com>
2025-12-02 10:41:25 +0800

    updated
    .

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,8 +6,9 @@ import YoutubeLineIcon from "remixicon-react/YoutubeLineIcon";
 
 const Footer = () => {
   return (
-    <footer className="  bg-white  ">
-      <div className="container m-auto text-center p-4 md:p-8 lg:p-10">
+    <footer className="relative bg-[#080812] w-full h-[600px] pt-[100px]">
+      <div className="absolute inset-0 -top-[920px] bg-[url('/v2/bg-pattern-wave2.png')] bg-cover bg-no-repeat z-0"></div>
+      <div className="bg-[rgba(61,78,108,0.15)] container m-auto text-center py-4 md:p-8 lg:p-10 h-[400px]">
         <div className="flex flex-wrap md:flex-row items-center gap-2 py-14">
           <span className="inline-flex gap-2 items-center px-2">
             <h3 className="font-bold col-span-2 text-xl uppercase">
