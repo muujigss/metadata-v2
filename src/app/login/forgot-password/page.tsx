689ae68ceab5d043d6c7c3c59f2693134ce75efe
muujig-qwer <79839772+muujig-qwer@users.noreply.github.com>
2025-12-10 23:44:35 +0800

    update login forgot

--- src/app/login/forgot-password/page.tsx
+++ src/app/login/forgot-password/page.tsx
@@ -5,7 +5,7 @@ import { Divider, Paper, Typography } from "@mui/material";
 
 const ForgotPasswordPage = () => {
   return (
-    <div className="flex w-full min-h-screen items-start justify-center overflow-hidden relative pt-2 sm:pt-10">
+    <div className="flex w-full min-h-screen items-start justify-center overflow-hidden relative pt-8 sm:pt-10">
       <div className="w-full max-w-md p-4 sm:p-8 rounded-2xl bg-[#3D4E6C33] backdrop-blur-sm shadow-xl border border-gray-700 z-10">
         <Typography className="uppercase text-text-title-medium mb-3 sm:mb-6 font-bold text-center text-white">
           Нууц үг мартсан?
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ import { Divider, Typography } from "@mui/material";
 
 const LoginPage = () => {
   return (
-    <div className="flex w-full min-h-screen items-start justify-center overflow-hidden relative pt-2 sm:pt-10">
+    <div className="flex w-full min-h-screen items-start justify-center overflow-hidden relative pt-8 sm:pt-10">
       <div className="w-full max-w-md p-4 sm:p-8 rounded-2xl bg-[#3D4E6C33] backdrop-blur-sm shadow-xl border border-gray-700 z-10">
         <Typography className="uppercase text-text-title-medium mb-3 sm:mb-6 font-bold text-center text-white">
           Системд Нэвтрэх
