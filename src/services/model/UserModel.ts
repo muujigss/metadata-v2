eaa3634763473254f50197ff20e7c82b6df17e67
ssmunkhbat <47767779+ssmunkhbat@users.noreply.github.com>
2025-12-10 21:40:40 +0800

    Update UserModel.ts
    .

--- src/services/model/UserModel.ts
+++ src/services/model/UserModel.ts
@@ -74,7 +74,7 @@ const loginUserModel = async (email: string, password: string) => {
     const user = await prisma.md_users.findFirst({
       where: {
         email,
-        is_active: true,
+        // is_active: true,
       },
       select: {
         id: true,
