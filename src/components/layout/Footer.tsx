105fddf2096b3c99ed9d620dcc376ce1aa688c27
muujig-qwer <79839772+muujig-qwer@users.noreply.github.com>
2025-12-10 10:11:21 +0800

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,18 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
-import FacebookFillIcon from "remixicon-react/FacebookFillIcon";
-import InstagramLineIcon from "remixicon-react/InstagramLineIcon";
-import YoutubeLineIcon from "remixicon-react/YoutubeLineIcon";
+
 
 const Footer = () => {
   return (
     <footer className="relative bg-[#080812] w-full h-[600px] pt-[100px]">
       <div className="absolute inset-0 -top-[920px] bg-[url('/v2/bg-pattern-wave2.png')] bg-cover bg-no-repeat z-0"></div>
-      <div className="bg-[rgba(61,78,108,0.15)] rounded-lg container m-auto text-center py-4 px-40 h-[400px] flex">
+      <div className="relative z-10 bg-[rgba(61,78,108,0.15)] rounded-lg container m-auto text-center py-4 px-40 h-[400px] flex">
         <div className="w-full flex justify-between items-center px-10">
           {/* Left: Ministry */}
           <div className="flex flex-col items-start gap-4 w-1/3">
-            <div className="flex items-center gap-4">
+            <Link
+              href="https://mddic.gov.mn"
+              target="_blank"
+              className="flex items-center gap-4 hover:opacity-80 transition-opacity"
+            >
               <Image
                 className="object-contain"
                 width={80}
@@ -26,47 +28,31 @@ const Footer = () => {
                 Цахим хөгжил, инновац, <br />
                 харилцаа холбооны яам
               </h3>
-            </div>
+            </Link>
           </div>
 
           {/* Center: e-Mongolia */}
           <div className="flex justify-center w-1/3">
-            <Image
-              src="/logo/E-Mongolia_Logo.png"
-              alt="e-Mongolia"
-              width={280}
-              height={112}
-              className="object-contain"
-            />
+            <Link href="https://e-mongolia.mn/" target="_blank">
+              <Image
+                src="/logo/E-Mongolia_Logo.png"
+                alt="e-Mongolia"
+                width={280}
+                height={112}
+                className="object-contain hover:opacity-80 transition-opacity"
+              />
+            </Link>
           </div>
 
-          {/* Right: Socials */}
-          <div className="flex flex-col items-end gap-5 w-1/3">
-            <div className="flex gap-4">
-              <Link
-                href={"https://www.facebook.com/StatisticMGL"}
-                target="_blank"
-                className="hover:opacity-80 transition-opacity"
-              >
-                <FacebookFillIcon color="white" size={32} />
-              </Link>
-              <Link
-                href={"https://www.youtube.com/@user-kz7vr8ke8k"}
-                target="_blank"
-                className="hover:opacity-80 transition-opacity"
-              >
-                <YoutubeLineIcon color="white" size={32} />
-              </Link>
-              <Link
-                href={
-                  "https://www.instagram.com/statisticsmgl?igsh=c2ljNm41dGxuMXdl"
-                }
-                target="_blank"
-                className="hover:opacity-80 transition-opacity"
-              >
-                <InstagramLineIcon color="white" size={32} />
-              </Link>
-            </div>
+          {/* Right: 1212.mn */}
+          <div className="flex flex-col items-center justify-center w-1/3 h-full">
+            <Link
+              href="https://www.1212.mn"
+              target="_blank"
+              className="text-white text-[36px] font-medium leading-tight hover:opacity-80 transition-opacity"
+            >
+              www.1212.mn
+            </Link>
           </div>
         </div>
       </div>
